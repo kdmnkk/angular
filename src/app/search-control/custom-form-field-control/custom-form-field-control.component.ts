c33c502a918c57c1d166b24ea6a4374e0e5d99a6
Anastasia <kdmnk.an@gmail.com>
2022-12-24 15:56:20 +0200

    feat: implement matFormFieldControl in CustomFormFieldControlComponent

--- src/app/search-control/custom-form-field-control/custom-form-field-control.component.ts
+++ src/app/search-control/custom-form-field-control/custom-form-field-control.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { NgControl } from '@angular/forms';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { Observable } from 'rxjs';
+
+export interface FormFieldValueInterface {
+  query: string;
+  scope: string;
+}
 
 @Component({
   selector: 'app-custom-form-field-control',
   templateUrl: './custom-form-field-control.component.html',
-  styleUrls: ['./custom-form-field-control.component.scss']
+  styleUrls: ['./custom-form-field-control.component.scss'],
+  providers: [{
+    provide: MatFormFieldControl,
+    useExisting: CustomFormFieldControlComponent,
+  }],
 })
-export class CustomFormFieldControlComponent {
+export class CustomFormFieldControlComponent implements MatFormFieldControl<FormFieldValueInterface> {
+  constructor() {}
+
+  autofilled!: boolean;
+  controlType!: string;
+  userAriaDescribedBy!: string;
+  disabled!: boolean;
+  empty!: boolean;
+  errorState!: boolean;
+  focused!: boolean;
+  id!: string;
+  ngControl!: NgControl | null;
+  placeholder!: string;
+  required!: boolean;
+  shouldLabelFloat!: boolean;
+  stateChanges!: Observable<void>;
+  value!: FormFieldValueInterface | null;
+
+
+  onContainerClick(event: MouseEvent): void {
+  }
+
+  setDescribedByIds(ids: string[]): void {
+  }
 
 }
